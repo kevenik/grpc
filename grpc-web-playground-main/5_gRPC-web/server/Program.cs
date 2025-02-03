443fe42cea36a51237366ccf9c3e37d57454dcff
KARTHIK <KARTHIK@PROPHZ.com>
2025-02-03 14:06:13 +0530

    first commit

---
+++ grpc-web-playground-main/5_gRPC-web/server/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using server.Services;
+using System.Net;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpcReflection();
+builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 5000, o => o.Protocols = HttpProtocols.Http1AndHttp2);
+});
+
+builder.Logging.AddConsole(); // Enable console logging
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+app.UseCors("AllowAll"); // Ensure CORS is applied first
+app.UseExceptionHandler(errorApp => // Add error handling
+{
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync("An unexpected error occurred.");
+    });
+});
+
+app.MapGrpcService<GreeterService>();
+app.MapGrpcReflectionService();
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
---
+++ grpc-web-playground-main/README.md
@@ -0,0 +1,20 @@
+# A Vulnerable gRPC-Web Application for Bug Hunters
+gRPC is an open-source, high-performance Remote Procedure Call (RPC) framework developed by Google. It enables efficient communication between client and server applications in a wide range of programming languages by utilizing Protocol Buffers (protobuf) for service definition and message serialization. The main goal of this repo is to learn about the gRPC communication patterns and hunt for vulnerabilities in the gRPC-Web app to improve your hunting skills
+
+# gRPC Communication Patterns
+
+- [Unary](https://github.com/bnematzadeh/grpc-web-playground/tree/main/1_Unary)
+- [Server Streaming](https://github.com/bnematzadeh/grpc-web-playground/tree/main/2_Server%20Streaming)
+- [Client Streaming](https://github.com/bnematzadeh/grpc-web-playground/tree/main/3_Client%20Streaming)
+- [Bi-Directional](https://github.com/bnematzadeh/grpc-web-playground/tree/main/4_Bi-Directional)
+
+# Vulnerable gRPC-Web Application
+
+![Logo](https://github.com/bnematzadeh/grpc-web-playground/blob/main/5_gRPC-web/grpc-web.png)
+
+I developed a vulnerable gRPC-Web application with the following vulnerabilities:
+
+- Injection
+- Information Disclosure
+- SSRF
+- Integer Overflow
